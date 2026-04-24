320761770cc54366e63c86429811f4cf53bc8011
thilinadee <thilina@domedia.lk>
2026-04-24 16:37:25 +0530

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,6 +11,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'My Site',
+  url: 'https://docloud-dev.github.io',
+  baseUrl: '/docloud-docs-public/',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -20,10 +22,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  //url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  //baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
